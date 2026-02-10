feb2108d983812b2655a8f9cc7499c1fc3677416
dfskaljf8 <ksurepalli259@gmail.com>
2026-02-10 17:09:44 -0500

    Fix eas branch push rejection

--- react-native-app/android/app/src/main/java/com/liftlinkapp/MainApplication.java
+++ react-native-app/android/app/src/main/java/com/liftlinkapp/MainApplication.java
@@ -9,7 +9,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
-import com.facebook.soloader.OpenSourceMergedSoMapping;
+import com.facebook.react.soloader.OpenSourceMergedSoMapping;
 import com.facebook.soloader.SoLoader;
 
 import java.util.List;
@@ -59,7 +59,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        SoLoader.init(this, OpenSourceMergedSoMapping);
+        SoLoader.init(this, OpenSourceMergedSoMapping.INSTANCE);
 
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             DefaultNewArchitectureEntryPoint.load();
